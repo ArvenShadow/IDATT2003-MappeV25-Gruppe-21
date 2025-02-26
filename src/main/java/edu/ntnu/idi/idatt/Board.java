ee02a2f90b8c98804ad0d1f1a1eef7d37dbb45ec
ArvenShadow <ole.markus@prolek.no>
2025-02-26 18:42:45 +0100

    Updated Board class with simple fields

--- src/main/java/edu/ntnu/idi/idatt/Board.java
+++ src/main/java/edu/ntnu/idi/idatt/Board.java
@@ -1,4 +1,19 @@
 package edu.ntnu.idi.idatt;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
+  private final Map<Integer, Tile> tiles;
+
+  public Board() {
+    tiles = new HashMap<>();
+  }
+
+  public void addTile(Tile tile) {
+    tiles.put(tile.getTileId(), tile);
+  }
+  public Tile getTile(int tileId) {
+    return tiles.get(tileId);
+  }
 }
