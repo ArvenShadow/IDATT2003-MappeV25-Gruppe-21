c74dfc26049444b8dd85b45499ca77763563994f
ArvenShadow <ole.markus@prolek.no>
2025-02-26 20:20:17 +0100

    BoardGame implementation (Very experimental)

--- src/main/java/edu/ntnu/idi/idatt/BoardGame.java
+++ src/main/java/edu/ntnu/idi/idatt/BoardGame.java
@@ -1,7 +1,41 @@
 package edu.ntnu.idi.idatt;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BoardGame {
+  private Board board;
+  private Player currentPlayer;
+  private List<Player> players;
+  private Dice dice;
+
+  public BoardGame() {
+    this.board = new Board();
+    this.players = new ArrayList<>();
+  }
+
+  public void addPlayer(Player player) {
+    players.add(player);
+  }
+
+  public void createBoard() {
+    for (int i = 1; i <= 100; i++) {
+      Tile tile = new Tile(i);
+      if (i == 5) {
+        tile.setTileAction(new LadderAction(10));
+      } else if (i == 11) {
+        tile.setTileAction(new ChuteAction(6));
+      }
+      board.addTile(tile);
+    }
+  }
+
+  public void createDice() {
+    this.dice = new Dice(1);
+  }
+
+  public Board getBoard() {
+    return board;
+  }
 
 }
