39a366dd63b3ce63ad0e7c25c39e677f45e43e5a
ArvenShadow <ole.markus@prolek.no>
2025-02-27 14:06:16 +0100

    GameFrame created for JavaFX

---
+++ src/main/java/edu/ntnu/idi/idatt/gui/GameFrame.java
@@ -0,0 +1,19 @@
+package edu.ntnu.idi.idatt.gui;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+
+public class GameFrame extends Application{
+
+  @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Label label = new Label("Hello World!");
+        StackPane root = new StackPane();
+        root.getChildren().add(label);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
