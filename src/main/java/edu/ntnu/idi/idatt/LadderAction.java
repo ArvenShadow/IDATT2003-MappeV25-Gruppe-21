18ba59052d1b62e29e0f2beca75db384e590955d
ArvenShadow <ole.markus@prolek.no>
2025-03-25 20:06:10 +0100

    Improved ladder action to handle both chute and ladder

--- src/main/java/edu/ntnu/idi/idatt/LadderAction.java
+++ src/main/java/edu/ntnu/idi/idatt/LadderAction.java
@@ -3,18 +3,40 @@ package edu.ntnu.idi.idatt;
 public class LadderAction implements TileAction{
 
   private int destinationTileId;
+  private boolean isLadder; //True for up, false for down
 
   public LadderAction(int destinationTileId){
     this.destinationTileId = destinationTileId;
+    this.isLadder = true;
   }
 
   @Override
   public void perform (Player player) {
-    System.out.println(player.getName() + " ladders to tile " + destinationTileId);
-    Tile destinationTile = player.getGame().getBoard().getTile(destinationTileId);
+    int startTileId = player.getCurrentTile().getTileId();
 
+    isLadder = destinationTileId > startTileId;
+
+    String actionType = isLadder ? "climbs ladder" : "slides down chute";
+
+    System.out.println(player.getName() + " " + actionType + "to tile " + destinationTileId);
+
+    Tile destinationTile = player.getGame().getBoard().getTile(destinationTileId);
     if (destinationTile != null) {
+      Tile oldTile = player.getCurrentTile();
+
       player.placeOnTile(destinationTile);
+
+      GameEvent.EventType eventType = isLadder ?
+        GameEvent.EventType.LADDER_CLIMBED :
+        GameEvent.EventType.CHUTE_SLID;
+
+
+      BoardGame game = player.getGame();
+      if (game instanceof ObservableGame) {
+        ((ObservableGame) game).notifyObservers(
+          new GameEvent(eventType, player, oldTile, destinationTile)
+        );
+      }
     } else {
       throw new IllegalStateException("Destination tile " + destinationTileId + " not found");
     }
