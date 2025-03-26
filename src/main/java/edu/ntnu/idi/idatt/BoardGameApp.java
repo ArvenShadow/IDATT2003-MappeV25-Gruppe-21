f758394d19847db782453161f13fc07a404e46b0
Jostrod <jostroda@stud.ntnu.no>
2025-03-26 14:49:09 +0100

    fix comment

--- src/main/java/edu/ntnu/idi/idatt/BoardGameApp.java
+++ src/main/java/edu/ntnu/idi/idatt/BoardGameApp.java
@@ -47,6 +47,8 @@ public class BoardGameApp {
     placePlayers();
   }
 
+
+  // Move this to scannerHandler eventually
   private void oldOrNewPlayers() {
     try {
       PlayerCsvHandler csvHandler = new PlayerCsvHandler(boardGame);
