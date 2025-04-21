b410aa99c0273a95af75d064012f678809c66ab6
Jostrod <jostroda@stud.ntnu.no>
2025-04-21 22:32:49 +0200

    Create main menu GUI

--- src/main/java/edu/ntnu/idi/idatt/ui/BoardGameApp.java
+++ src/main/java/edu/ntnu/idi/idatt/ui/BoardGameApp.java
@@ -3,136 +3,38 @@ package edu.ntnu.idi.idatt.ui;
 import edu.ntnu.idi.idatt.model.BoardGame;
 import edu.ntnu.idi.idatt.model.Player;
 import edu.ntnu.idi.idatt.model.Tile;
+import edu.ntnu.idi.idatt.view.mainMenu.MainMenu;
+import edu.ntnu.idi.idatt.view.mainMenu.MenuController;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
 import java.util.Scanner;
 
-public class BoardGameApp {
+public class BoardGameApp extends Application {
+
+  public static Stage stage;
   private BoardGame boardGame;
   private Scanner scanner;
 
+
   public BoardGameApp() {
-    this.boardGame = new BoardGame();
-    this.scanner = new Scanner(System.in);
+
   }
 
-  public void start() {
-    System.out.println("Welcome to Snakes and Ladders!");
+  @Override
+  public void start(Stage primaryStage) {
 
-    // Initialize game
-    initializeGame();
+    stage = primaryStage;
 
-    // Game loop
-    playGame();
 
-    // Display winner and cleanup
-    endGame();
+    MenuController menuController = new MenuController();
 
-    scanner.close();
-  }
 
-  private void initializeGame() {
-    // Create board (using factory)
-    boardGame.createBoard();
-
-    // Create dice
-    boardGame.createDice();
-
-    // Add players
-    addPlayers();
-
-    // Place all players on the first tile
-    placePlayers();
-  }
-
-  private void addPlayers() {
-    System.out.print("Enter number of players (2-4): ");
-    int numPlayers = getIntInput(2, 4);
-
-    String[] tokenTypes = {"TopHat", "RaceCar", "Dog", "Ship"};
-
-    for (int i = 0; i < numPlayers; i++) {
-      System.out.print("Enter name for Player " + (i+1) + ": ");
-      String name = scanner.nextLine();
-
-      System.out.println("Choose token type:");
-      for (int j = 0; j < tokenTypes.length; j++) {
-        System.out.println((j+1) + ". " + tokenTypes[j]);
-      }
-      System.out.print("Enter choice (1-" + tokenTypes.length + "): ");
-
-      int tokenChoice = getIntInput(1, tokenTypes.length);
-      String tokenType = tokenTypes[tokenChoice-1];
-
-      Player player = new Player(name, boardGame, tokenType);
-      boardGame.addPlayer(player);
-
-      System.out.println(name + " will use the " + tokenType + " token.");
-    }
-  }
-
-  private void placePlayers() {
-    Tile startTile = boardGame.getBoard().getTile(1);
-    for (Player player : boardGame.getPlayers()) {
-      player.placeOnTile(startTile);
-    }
-  }
-
-  private void playGame() {
-    int roundNumber = 1;
-
-    while (!boardGame.isFinished()) {
-      System.out.println("\n=== Round " + roundNumber + " ===");
-      displayPlayerPositions();
-
-      System.out.println("\nPress Enter to play round...");
-      scanner.nextLine();
-
-      boardGame.playOneRound();
-
-      roundNumber++;
-    }
-  }
-
-  private void displayPlayerPositions() {
-    for (Player player : boardGame.getPlayers()) {
-      Tile currentTile = player.getCurrentTile();
-      int position = currentTile != null ? currentTile.getTileId() : 0;
-      System.out.println(player.getName() + " (" + player.getTokenType() +
-        ") is on tile " + position);
-    }
-  }
-
-  private void endGame() {
-    System.out.println("\n=== Game Over ===");
-    System.out.println("The winner is: " + boardGame.getWinner().getName() +
-      " (" + boardGame.getWinner().getTokenType() + ")");
-  }
-
-  private int getIntInput(int min, int max) {
-    int choice = min;
-    boolean validInput = false;
-
-    while (!validInput) {
-      try {
-        String input = scanner.nextLine();
-        choice = Integer.parseInt(input);
-
-        if (choice >= min && choice <= max) {
-          validInput = true;
-        } else {
-          System.out.print("Please enter a number between " + min +
-            " and " + max + ": ");
-        }
-      } catch (NumberFormatException e) {
-        System.out.print("Invalid input. Please enter a number: ");
-      }
-    }
-
-    return choice;
+    MainMenu mainMenu = new MainMenu(primaryStage, menuController);
+    mainMenu.show();
   }
 
   public static void main(String[] args) {
-    BoardGameApp app = new BoardGameApp();
-    app.start();
+    launch(args);
   }
 }
---
+++ src/main/java/edu/ntnu/idi/idatt/view/mainMenu/MenuController.java
@@ -0,0 +1,29 @@
+package edu.ntnu.idi.idatt.view.mainMenu;
+
+public class MenuController {
+
+    public void startChutesAndLadders(){
+        System.out.println("Starting chutes and ladders...");
+    }
+
+    public void startTicTacToe(){
+        System.out.println("Starting tic tac toe...");
+    }
+
+    public void startPong(){
+        System.out.println("Starting pong...");
+    }
+
+    public void startConncectFour(){
+        System.out.println("Starting connect four...");
+    }
+
+    public void startHangman(){
+        System.out.println("Starting hangman...");
+    }
+
+    public void exit(){
+        System.out.println("Exiting...");
+        System.exit(0);
+    }
+}
