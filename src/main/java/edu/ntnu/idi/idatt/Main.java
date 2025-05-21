c8290b48caec5f20ac463c31b2100edd6d790074
Jostrod <jostroda@stud.ntnu.no>
2025-05-21 23:50:50 +0200

    Create Board selection view

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -2,25 +2,32 @@ package edu.ntnu.idi.idatt;
 
 import edu.ntnu.idi.idatt.navigation.NavTo;
 import edu.ntnu.idi.idatt.navigation.NavigationManager;
+import edu.ntnu.idi.idatt.view.BoardSelectionView;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
-    // Initialize navigationManager manager
-    NavigationManager navManager = NavigationManager.getInstance();
-    navManager.initialize(primaryStage, "Snakes and Ladders", 1300, 900);
+    // Fjern koden som navigerer til START_SCREEN
+    /* NavigationManager navManager = NavigationManager.getInstance();
+       navManager.initialize(primaryStage, "Snakes and Ladders", 1300, 900);
+       navManager.navigateTo(NavTo.START_SCREEN);
+    */
 
-    // Set up CSS styling
-    String cssPath = getClass().getResource("/styles/game.css").toExternalForm();
-    primaryStage.getScene().getStylesheets().add(cssPath);
+    // Sett opp BoardSelectionView som første visning
+    BoardSelectionView boardSelectionView = new BoardSelectionView();
+    Scene scene = boardSelectionView.getStyledScene();
 
-    // Navigate to start screen
-    navManager.navigateTo(NavTo.START_SCREEN);
-
+    // Sett scenen og vis vinduet
+    primaryStage.setScene(scene);
+    primaryStage.setTitle("Board Selection");
+    primaryStage.setFullScreen(true);
+    primaryStage.setFullScreenExitHint("");
     primaryStage.show();
   }
+
 
   public static void main(String[] args) {
     launch(args);
