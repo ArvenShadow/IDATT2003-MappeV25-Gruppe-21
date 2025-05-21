67fd2085ea5f51a0215e75fdc1885468956a3f79
Jostrod <jostroda@stud.ntnu.no>
2025-05-22 00:27:59 +0200

    Connect celect-board-buttons to Json-files

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -10,17 +10,17 @@ import javafx.stage.Stage;
 public class Main extends Application {
   @Override
   public void start(Stage primaryStage) {
-    // Fjern koden som navigerer til START_SCREEN
+
     /* NavigationManager navManager = NavigationManager.getInstance();
        navManager.initialize(primaryStage, "Snakes and Ladders", 1300, 900);
        navManager.navigateTo(NavTo.START_SCREEN);
     */
 
-    // Sett opp BoardSelectionView som første visning
+
     BoardSelectionView boardSelectionView = new BoardSelectionView();
     Scene scene = boardSelectionView.getStyledScene();
 
-    // Sett scenen og vis vinduet
+
     primaryStage.setScene(scene);
     primaryStage.setTitle("Board Selection");
     primaryStage.setFullScreen(true);
