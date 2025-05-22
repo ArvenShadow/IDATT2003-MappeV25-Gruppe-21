30a1a42614029de38d8f57457a8dadb1bc04af7f
ArvenShadow <ole.markus@prolek.no>
2025-05-22 23:33:54 +0200

    unnecessary imports removed

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -2,10 +2,7 @@ package edu.ntnu.idi.idatt;
 
 import edu.ntnu.idi.idatt.navigation.NavTo;
 import edu.ntnu.idi.idatt.navigation.NavigationManager;
-import edu.ntnu.idi.idatt.view.BoardSelectionView;
-import edu.ntnu.idi.idatt.view.MainMenuView;
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
