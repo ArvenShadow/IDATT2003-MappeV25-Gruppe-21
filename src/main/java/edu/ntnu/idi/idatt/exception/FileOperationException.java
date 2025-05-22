f50e0922eb1a8f88e89b1d8d9bdd13ca39f9e675
ArvenShadow <ole.markus@prolek.no>
2025-05-23 00:26:24 +0200

    Remove unused exception handling classes

--- src/main/java/edu/ntnu/idi/idatt/exception/FileOperationException.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.idi.idatt.exception;
-
-public class FileOperationException extends BoardGameException {
-  public FileOperationException(String message, Throwable cause) {
-    super(message, cause);
-  }
-}
--- src/main/java/edu/ntnu/idi/idatt/exception/InvalidMoveException.java
+++
@@ -1,7 +0,0 @@
-package edu.ntnu.idi.idatt.exception;
-
-public class InvalidMoveException extends BoardGameException {
-  public InvalidMoveException(String message) {
-    super(message);
-  }
-}
