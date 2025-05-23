0306ebe5ad21dcc3e4784c4e3509fcd40ab4b3ac
ArvenShadow <ole.markus@prolek.no>
2025-05-23 03:22:48 +0200

    updated pom.xml and added launcher class

---
+++ src/main/java/edu/ntnu/idi/idatt/Launcher.java
@@ -0,0 +1,22 @@
+package edu.ntnu.idi.idatt;
+
+/**
+ * Launcher class that serves as the application entry point.
+ * This class delegates to the main application.
+ */
+public class Launcher {
+
+  /**
+   * Default constructor for Launcher.
+   */
+  public Launcher() {
+
+  }
+  /**
+   * Main method that launches the Main application.
+   *
+   * @param args command line arguments
+   */
+  public static void main(String[] args) {Main.main(args);
+  }
+}
