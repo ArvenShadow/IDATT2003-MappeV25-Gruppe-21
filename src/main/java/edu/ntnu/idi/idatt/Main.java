639ac265bd9bbbf7fe67bce905c8503775420458
ArvenShadow <ole.markus@prolek.no>
2025-05-23 02:44:06 +0200

    Javadoc to main class

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -5,6 +5,16 @@ import edu.ntnu.idi.idatt.navigation.NavigationManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+/**
+ * The Main class serves as the entry point for the JavaFX application.
+ * It initializes the primary stage and sets up the main navigation flow
+ * for the application using the NavigationManager singleton. The application
+ * begins by navigating to the START_SCREEN.
+ *
+ * <p>This class extends the {@code Application} class, which is the base class
+ * for JavaFX application development. The {@code start} method is overridden
+ * to configure the application's primary stage.
+ */
 public class Main extends Application {
   @Override
 
